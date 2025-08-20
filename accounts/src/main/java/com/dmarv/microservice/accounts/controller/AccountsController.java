4b6a00aa4604d745a0149abd2da6418c6e293987
timothy <timothy@dmarv.dmarv.com>
2025-08-20 11:23:54 +0300

    Added Exception Handling and all

--- accounts/src/main/java/com/dmarv/microservice/accounts/controller/AccountsController.java
+++ accounts/src/main/java/com/dmarv/microservice/accounts/controller/AccountsController.java
@@ -3,6 +3,8 @@ package com.dmarv.microservice.accounts.controller;
 import com.dmarv.microservice.accounts.constants.AccountsConstants;
 import com.dmarv.microservice.accounts.dto.CustomerDto;
 import com.dmarv.microservice.accounts.dto.ResponseDto;
+import com.dmarv.microservice.accounts.service.IAccountsService;
+import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -13,10 +15,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(path = "/api", produces = {MediaType.APPLICATION_JSON_VALUE})
+@AllArgsConstructor
 public class AccountsController {
+    private IAccountsService iAccountsService;
 
     @PostMapping("/create")
     public ResponseEntity<ResponseDto> createAccount(@RequestBody CustomerDto customerDto) {
+        iAccountsService.createAccount(customerDto);
         return ResponseEntity.status(HttpStatus.CREATED).body(new ResponseDto(AccountsConstants.STATUS_201, AccountsConstants.MESSAGE_201));
     }
 
--- accounts/src/main/java/com/dmarv/microservice/accounts/service/impl/AccountsServiceImpl.java
+++ accounts/src/main/java/com/dmarv/microservice/accounts/service/impl/AccountsServiceImpl.java
@@ -1,11 +1,20 @@
 package com.dmarv.microservice.accounts.service.impl;
 
+import com.dmarv.microservice.accounts.constants.AccountsConstants;
 import com.dmarv.microservice.accounts.dto.CustomerDto;
+import com.dmarv.microservice.accounts.entity.Accounts;
+import com.dmarv.microservice.accounts.entity.Customer;
+import com.dmarv.microservice.accounts.exception.CustomerAlreadyExistsException;
+import com.dmarv.microservice.accounts.mapper.CustomerMapper;
 import com.dmarv.microservice.accounts.repository.AccountsRepository;
 import com.dmarv.microservice.accounts.repository.CustomerRepository;
 import com.dmarv.microservice.accounts.service.IAccountsService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+import java.util.Random;
 
 @Service
 @AllArgsConstructor
@@ -15,7 +24,31 @@ public class AccountsServiceImpl implements IAccountsService {
 
     @Override
     public void createAccount(CustomerDto customerDto) {
-
+        Customer customer = CustomerMapper.mapToCustomer(customerDto, new Customer());
+        Optional<Customer> optionalCustomer =
+                customerRepository.findByMobileNumber(customerDto.getMobileNumber());
+        if (optionalCustomer.isPresent()) {
+            throw new CustomerAlreadyExistsException("A Customer was already registered" +
+                    " with the given mobile number:  " + customerDto.getMobileNumber());
+        }
+        customer.setCreatedAt(LocalDateTime.now());
+        customer.setCreatedBy("Anonymous");
+        Customer savedCustomer = customerRepository.save(customer);
+        accountsRepository.save(createNewAccount(savedCustomer));
 
     }
+
+    private Accounts createNewAccount(Customer customer) {
+        Accounts newAccount = new Accounts();
+        newAccount.setCustomerId(customer.getCustomerId());
+        long randomAccNumber = 1000000000L + new Random().nextInt(900000000);
+
+        newAccount.setAccountNumber(randomAccNumber);
+        newAccount.setAccountType(AccountsConstants.SAVINGS);
+        newAccount.setBranchAddress(AccountsConstants.ADDRESS);
+        newAccount.setCreatedAt(LocalDateTime.now());
+        newAccount.setCreatedBy("Anonymous");
+        return newAccount;
+    }
+
 }
