08cb491725b29ca60c98097fce50a75d0f72236b
OWINY-DOLLO MARVIN <owinymarvin@gmail.com>
2025-08-20 17:50:36 +0300

    Added input data validation for GET, POST, PUT, DELETE requests

--- accounts/src/main/java/com/dmarv/microservice/accounts/dto/CustomerDto.java
+++ accounts/src/main/java/com/dmarv/microservice/accounts/dto/CustomerDto.java
@@ -1,11 +1,24 @@
 package com.dmarv.microservice.accounts.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class CustomerDto {
+    @NotEmpty(message = "Name cannot be empty")
+    @Size(min = 5, max = 30, message = "Name must be between 5 and 30 characters")
     private String name;
+
+    @NotEmpty(message = "Email cannot be empty")
+    @Email(message = "Email must be a valid email address")
     private String email;
+
+    @NotEmpty(message = "Mobile number cannot be empty")
+    @Pattern(regexp = "^[0-9]{10}$", message = "Mobile number must be 10 digits")
     private String mobileNumber;
+    
     private AccountsDto accountsDto;
 }
--- accounts/src/main/java/com/dmarv/microservice/accounts/exception/GlobalExceptionHandler.java
+++ accounts/src/main/java/com/dmarv/microservice/accounts/exception/GlobalExceptionHandler.java
@@ -1,16 +1,43 @@
 package com.dmarv.microservice.accounts.exception;
 
 import com.dmarv.microservice.accounts.dto.ErrorResponseDto;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @ControllerAdvice
-public class GlobalExceptionHandler {
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+    // default ResponseEntityExceptionHandler method is overridden
+    // to change how it was defined.
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException ex, HttpHeaders headers
+            , HttpStatusCode status, WebRequest request) {
+        Map<String, String> validationErrors = new HashMap<>();
+        List<ObjectError> validationErrorsList =
+                ex.getBindingResult().getAllErrors();
+
+        validationErrorsList.forEach(error -> {
+            String fieldName = ((FieldError) error).getField();
+            String validationErrorMessage = error.getDefaultMessage();
+            validationErrors.put(fieldName, validationErrorMessage);
+        });
+        return new ResponseEntity<>(validationErrors, HttpStatus.BAD_REQUEST);
+    }
+
     // User defined exceptions here
     @ExceptionHandler(CustomerAlreadyExistsException.class)
     public ResponseEntity<ErrorResponseDto> handleCustomerAlreadyExistsException(CustomerAlreadyExistsException exception, WebRequest webRequest) {
