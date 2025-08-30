4d2dad11c13c3d259415c9e498c62919fed07d7d
OWINY-DOLLO MARVIN <owinymarvin@gmail.com>
2025-08-30 23:23:57 +0300

    OpenAPI documentation added

--- accounts/src/main/java/com/dmarv/microservice/accounts/AccountsApplication.java
+++ accounts/src/main/java/com/dmarv/microservice/accounts/AccountsApplication.java
@@ -1,11 +1,39 @@
 package com.dmarv.microservice.accounts;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Accounts Microservice REST API " +
+                        "Documentation",
+                description = "EazyBank Accounts Microservice " +
+                        "RESTAPI Documentation",
+                version = "v1.0.0",
+                contact = @Contact(
+                        name = "Owiny Marvin",
+                        email = "marvino@oliveeb.com",
+                        url = "https://oliveeb.com"
+                ),
+                license = @License(
+                        name = "Apache 2.0",
+                        url = "https://www.apache.org/licenses/LICENSE-2.0"
+                )
+        ),
+        externalDocs = @ExternalDocumentation(
+                description = "EasyBank Accounts Microservice REST API " +
+                        "Documentation",
+                url = "httpss://oliveeb.com/swagger-ui/index.html"
+        )
+)
 public class AccountsApplication {
 
     public static void main(String[] args) {
